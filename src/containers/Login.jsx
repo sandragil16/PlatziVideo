cb407a9095531f9d2e2b21ddb7a3a6ff4642e421
Rick C <rickcastaneda@outlook.com>
2020-12-02 01:59:53 -0600

    Solve issues with Login styling and redux

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -4,26 +4,36 @@ import googleIcon from '../assets/styles/static/google-icon.png';
 import twitterIcon from '../assets/styles/static/twitter-icon.png';
 
 const Login = () => (
-    <section className="login" >
-      <section className="login__container" />
-        <h2>Inicia sesión</h2>
-        <form className="login__container--form">
-          <input className="input" type="text" placeholder="Correo" />
-          <input className="input" type="password" placeholder="Contraseña" />
-          <button className="button">Iniciar sesión</button>
-          <div className="login__container--remember-me">
-           <label>
-             <input type="checkbox" id="cbox1" value="first_checkbox" /> Recuérdame
-           </label>
-           <a href="/">Olvidé mi contraseña</a>
-         </div>
+  <section className='login'>
+    <section className='login__container'>
+      <h2>Inicia sesión</h2>
+      <form className='login__container--form'>
+        <input className='input' type='text' placeholder='Correo' />
+        <input className='input' type='password' placeholder='Contraseña' />
+        <button type='button' className='button'>Iniciar sesión</button>
+        <div className='login__container--remember-me'>
+          <label htmlFor='cbox1'>
+            <input type='checkbox' id='cbox1' name='cbox1' value='first_checkbox' />
+            Recuérdame
+          </label>
+          <a href='/'>Olvidé mi contraseña</a>
+        </div>
       </form>
-        <section className="login__container--social-media">
-         <div><img src={googleIcon} /> Inicia sesión con Google</div>
-         <div><img src={twitterIcon} /> Inicia sesión con Twitter</div>
-        </section>
-       <p className="login__container--register">No tienes ninguna cuenta <a href=""> Regístrate</a></p>
-     </section >
+      <section className='login__container--social-media'>
+        <div>
+          <img src={googleIcon} alt='Google Icon' />
+          Inicia sesión con Google
+        </div>
+        <div>
+          <img src={twitterIcon} alt='Twitter Icon' />
+          Inicia sesión con Twitter
+        </div>
+      </section>
+      <p className='login__container--register'>
+        No tienes ninguna cuenta
+        <a href='/'> Regístrate</a>
+      </p>
+    </section>
   </section>
 );
 
