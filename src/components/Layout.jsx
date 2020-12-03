87ee4d28e0878612729faaaefb2c2bfc0efb5e3a
Sandra Gil <scgmendozaprivate16@gmail.com>
2020-12-02 23:06:16 -0600

    Introduce layout/children & not found text

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
-const Layout = () => (
+const Layout = ({ children }) => (
   <div className='App'>
     <Header />
-    {Children}
+    {children}
     <Footer />
   </div>
 );
